1a497c99f8e8c7cb3c5c8f5d352371f5042ded77
Nikolai Bondarenko <dnobless@mail.ru>
2023-05-24 20:12:23 +0300

    HW 16.04.23

---
+++ 2023.04.16/1.py
@@ -0,0 +1,23 @@
+
+num1 = float(input("Введите первое число: "))
+num2 = float(input("Введите второе число: "))
+num3 = float(input("Введите третье число: "))
+
+
+sum_positive = 0
+if num1 > 0:
+    sum_positive += num1
+if num2 > 0:
+    sum_positive += num2
+if num3 > 0:
+    sum_positive += num3
+
+
+print(sum_positive)
+
+
+#Введите первое число: 2
+#Введите второе число: -6
+#Введите третье число: 2.5
+#4.5
+
---
+++ 2023.04.16/3.py
@@ -0,0 +1,10 @@
+year = int(input("Введите год: "))
+
+if year % 4 == 0 or year % 400 == 0 and year % 100 != 0:
+    print("Да")
+else:
+    print("Нет")
+
+
+#Введите год: 2016
+#Да
